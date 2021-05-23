f81a9d8e45c176746ee83f7732c4a448f985d78d
Xabier Lameiro Cardama <xabier.lameirocardama@plexus.es>
2021-05-23 22:28:24 +0200

    Remove unnecessary test

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ export default function Home(): React.Element<React$FragmentType> {
     return (
         <>
             <h1>Index</h1>
+            <a href="/coverage/lcov-report/index.html">Coverage</a>
         </>
     )
 }
